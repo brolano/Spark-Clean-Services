aaef64e7976a279913cf3ae6185193ad57a81d5a
Brian Solano <brian_solano110012@yahoo.com>
2025-05-28 22:33:40 -0500

    implemented apollo to client side

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { ApolloProvider } from '@apollo/client';
 import './index.css'
 import App from './App.tsx'
+import { client } from './apolloClient';
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ApolloProvider client={client}>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </ApolloProvider>
 )
